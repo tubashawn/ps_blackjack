86956120519a8b6bd61dd12a6e14e2b364024eca
tubaShawn <shawnrmangum@gmail.com>
2018-12-12 21:54:17 -0700

    getting ps going again

--- assets/interactive.js
+++ assets/interactive.js
@@ -3,31 +3,22 @@ var cards = [];
 function createCards() {
     for (var i = 1; i <= 13; i++) {
         for (var j = 0; j < 4; j++) {
-            if (i == 1) {
-                cards.push("A");
-            } else if (i == 11) {
-                cards.push("J");
-            } else if(i == 12) {
-                cards.push("Q");
-            } else if (i == 13) {
-                cards.push("K");
-            }
+            // if (i == 1) {
+            //     cards.push("A");
+            // } else if (i == 11) {
+            //     cards.push("J");
+            // } else if(i == 12) {
+            //     cards.push("Q");
+            // } else if (i == 13) {
+            //     cards.push("K");
+            // }
+            // if statement vs switch statement. same result
             switch (i) {
-                case i == 1:
-                    cards.push("A");
-                    break;
-                case i == 11:
-                    cards.push("J");
-                    break;
-                case i == 12:
-                    cards.push("Q");
-                    break;
-                case i == 13:
-                    cards.push("K");
-                    break;
-                default:
-                    cards.push(i);
-                    break;
+                case i == 1: cards.push("A"); break;
+                case i == 11: cards.push("J"); break;
+                case i == 12: cards.push("Q"); break;
+                case i == 13: cards.push("K"); break;
+                default: cards.push(i); break;
             }
         }
     }
