cd90d46f1b51d4287722767e90aa81e59d828a26
tubaShawn <shawnrmangum@gmail.com>
2019-01-01 21:39:30 -0700

    added card object

--- assets/interactive.js
+++ assets/interactive.js
@@ -16,13 +16,20 @@ function createCards() {
             //     cards.push(j + " of " + suits[i]); 
             // }
             // if statement vs switch statement. same result
+            let card = {
+                suit: suits[i],
+                value: j
+            };
+
             switch (j) {
-                case 1: cards.push("A" + " of " + suits[i]); break;
-                case 11: cards.push("J" + " of " + suits[i]); break;
-                case 12: cards.push("Q" + " of " + suits[i]); break;
-                case 13: cards.push("K" + " of " + suits[i]); break;
-                default: cards.push(j + " of " + suits[i]); 
+                case 1: card.value = "Ace"; break;
+                case 11: card.value = "Jack"; break;
+                case 12: card.value = "Queen"; break;
+                case 13: card.value = "King"; break;
+                default: card.value = j; 
             }
+
+            cards.push(card);
         }
     }
 }
@@ -34,6 +41,6 @@ function getNextCard() {
 }
 
 
-let playerCards = [getNextCard(), getNextCard()];
-
+// let playerCards = [getNextCard(), getNextCard()];
+console.log(cards);
 console.log(playerCards);
