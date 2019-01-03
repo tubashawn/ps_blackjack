b34fd983754af0729900cff275bf1fb9a4eedb75
tubaShawn <shawnrmangum@gmail.com>
2019-01-02 22:00:20 -0700

    randomized deck

--- assets/interactive.js
+++ assets/interactive.js
@@ -22,23 +22,15 @@ function createCards() {
 
 createCards();
 
-function getNextCard() {
-    return cards.shift();
-}
-
-function getCardString(card) {
-    return card.value + " of " + card.suit;
-} 
 
 // DOM variables
 let textArea = document.getElementById("text-area"),
-    okButton = document.getElementById("ok"),
-    newGameButton = document.getElementById("new-game-button"),
+newGameButton = document.getElementById("new-game-button"),
     hitButton = document.getElementById("hit-button"),
     stayButton = document.getElementById("stay-button");
-
-// Game variables
-let gameStarted = false,
+    
+    // Game variables
+    let gameStarted = false,
     gameOver = false,
     playerWon = false,
     dealerCards = [],
@@ -47,20 +39,45 @@ let gameStarted = false,
     playerScore = 0,
     deck = [];
 
-hitButton.style.display = "none";
+    hitButton.style.display = "none";
 stayButton.style.display = "none";
-// showStatus();
+showStatus();
 
 newGameButton.addEventListener("click", function() {
-    textArea.innerText = "Let's begin!!!";
-
+    textArea.innerText = "It's only a game ;)";
+    
     gameStarted = true;
     gameOver = false;
     playerWon = false;
-
+    
     deck = cards;
-    console.log(deck);
+    shuffleDeck(deck);
     newGameButton.style.display = "none";
-    hitButton.style.display = "block";
-    stayButton.style.display = "block";
+    hitButton.style.display = "inline";
+    stayButton.style.display = "inline";
+    showStatus();
 });
+
+// Game functions
+function showStatus() {
+    if (!gameStarted) {
+        textArea.innerText = "Welcome to Blackjack! Let's begin!!!";
+    }
+}
+
+function shuffleDeck(deck) {
+    for (let i = 0; i < deck.length; i++) {
+        let swapIdx = Math.trunc(Math.random() * deck.length);
+        let temp = deck[swapIdx];
+        deck[swapIdx] = deck[i];
+        deck[i] = temp;
+    }
+}
+
+function getNextCard() {
+    return cards.shift();
+}
+
+function getCardString(card) {
+    return card.value + " of " + card.suit;
+} 
